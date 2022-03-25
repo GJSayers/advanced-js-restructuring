d75dffea8de96b283ef7a9bc2d149b825cffcae8
GJSayers <gemma.sayers1@btinternet.com>
2022-03-25 17:24:11 +0000

    Add training excercise

--- destructuring.js
+++ destructuring.js
@@ -56,3 +56,43 @@ let {brian, anna, ...rest} = favouriteFoods;
 console.log(brian);
 console.log(anna);
 console.log(rest);
+
+// challenge script:
+
+let students = [
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'John',
+        subjects: ['art', 'cad', 'english', 'maths', 'science'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 93, maths: 77, art: 95},
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 87, art: 95},
+    }
+];
+const makeList = (arr, student) => {
+    subjectArr = [];
+    for (let i in arr) {
+        if (student === arr[i].name) {
+            subjectArr = [...arr[i].subjects];
+        }
+    }
+    return subjectArr
+}
+let [first, second, ...remainder] = makeList(students, "John");
+console.log(first, second, remainder);
